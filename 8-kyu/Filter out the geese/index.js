0cd7cd137c113c8c8c84e6f663eeee5c18e68325
NarminGale <narmingale@gmail.com>
2021-11-02 17:51:01 +0400

    Solved new kata

---
+++ 8-kyu/Filter out the geese/index.js
@@ -0,0 +1,17 @@
+function gooseFilter(birds) {
+  var geese = ['African', 'Roman Tufted', 'Toulouse', 'Pilgrim', 'Steinbacher']
+
+  // return an array containing all of the strings in the input array except those that match strings in geese
+
+  let result = []
+
+  for (let i = 0; i < birds.length; i++) {
+    console.log(birds[i])
+
+    if (!geese.includes(birds[i])) {
+      result.push(birds[i])
+    }
+  }
+
+  return result
+}
